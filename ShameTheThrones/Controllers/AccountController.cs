a9b87f4d1205d10654520429729947695b8547a3
Rebecca Luttmer <rebecca.luttmer@gmail.com>
2016-02-07 13:29:10 -0500

    Deleting test

--- ShameTheThrones/Controllers/AccountController.cs
+++ ShameTheThrones/Controllers/AccountController.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using ShameTheThrones.Models;
-//Testing Git!
 
 namespace ShameTheThrones.Controllers
 {
