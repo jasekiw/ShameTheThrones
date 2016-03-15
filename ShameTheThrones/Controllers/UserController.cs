c6d29409062b040fb094c75a2b4b128a6518999a
travistme <travistme@gmail.com>
2016-03-14 23:48:19 -0400

    Add basic functionality to allow users to register for accounts.

---
+++ ShameTheThrones/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShameTheThrones.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShameTheThrones/Models/UserModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShameTheThrones.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public DateTime DeletedAt;
+
+        public void Register(UserModel user)
+        {
+            using (shamethethronesEntities db = new shamethethronesEntities())
+            {
+                User newUser = new User();
+
+                newUser.username = user.UserName;
+                newUser.email = user.Email;
+                newUser.pasword = user.Password;
+
+                db.Users.Add(newUser);
+                db.SaveChanges();
+            }
+        }
+
+        public void LogIn(UserModel user)
+        {
+            using (shamethethronesEntities db = new shamethethronesEntities())
+            {
+                // TODO: Stuff to log in and verify credentials
+            }
+        }
+        
+    }
+}
