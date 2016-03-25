db21d62a6038dc3898ac72620104fc5f4c9f336f
Rebecca Luttmer <rebecca.luttmer@gmail.com>
2016-03-24 21:59:15 -0400

    Added test classes with no body

---
+++ ShameTheThrones.Test/Models/BathroomModelTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShameTheThrones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShameTheThrones.Models.Tests
+{
+    [TestClass()]
+    public class BathroomModelTests
+    {
+        [TestMethod()]
+        public void AddBathroomTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
