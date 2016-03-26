b02fcd651053cb9f861db18c5d1646a0e028fcf3
d-paller <darthdollar@gmail.com>
2016-03-25 20:59:06 -0400

    Add IU Test

---
+++ ShameTheThrones.Test/UITest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace ShameTheThrones.Test
+{
+
+    [TestFixture]
+    public class Driver
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Create a new instance of the IE driver
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+
+
+       [Test]
+       // Tests to make sure values appear, not that they are correct
+       public void testSearch()
+        {
+            driver.Navigate().GoToUrl("http://localhost:60060/");
+
+            IWebElement search = driver.FindElement(By.TagName("input"));
+
+            search.SendKeys("40245"); // enter search query
+
+            IReadOnlyCollection <IWebElement> output = driver.FindElements(By.CssSelector(".result.row"));
+
+            Assert.AreNotEqual(output.Count, 0);
+        } // test search
+
+        public void testHomeButton()
+        {
+
+        } // test home button
+    } // driver
+}
