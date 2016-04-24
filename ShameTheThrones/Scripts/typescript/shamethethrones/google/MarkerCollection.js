196fa9b89318d08381481c04b0f9ac14288a632c
Rebecca Luttmer <rebecca.luttmer@gmail.com>
2016-04-24 16:24:51 -0400

    removed git tracking of js files

--- ShameTheThrones/Scripts/typescript/shamethethrones/google/MarkerCollection.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-});
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJnb29nbGUvTWFya2VyQ29sbGVjdGlvbi5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
--- ShameTheThrones/Scripts/typescript/shamethethrones/restroom/RestroomSearchObject.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-});
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJyZXN0cm9vbS9SZXN0cm9vbVNlYXJjaE9iamVjdC5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
