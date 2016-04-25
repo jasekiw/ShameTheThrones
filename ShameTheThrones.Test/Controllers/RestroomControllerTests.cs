ea8f15fbb17047ddfd8d76f766475a868bfde398
Rebecca Luttmer <rebecca.luttmer@gmail.com>
2016-04-24 20:28:03 -0400

    trying to get controller tests to work

--- ShameTheThrones.Test/Controllers/RestroomControllerTests.cs
+++ ShameTheThrones.Test/Controllers/RestroomControllerTests.cs
@@ -24,3 +24,33 @@ namespace ShameTheThrones.Controllers.Tests
         }
     }
 }
+/*
+        private Mock<shamethethronesEntities> _mockDb;
+        private Mock<Restroom> _mockRR1;
+        private Mock<Restroom> _mockRR2;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockDb = new Mock<shamethethronesEntities>();
+            _mockRR1 = new Mock<Restroom>();
+            _mockRR2 = new Mock<Restroom>();
+            _mockDb.SetupAllProperties();
+            _mockRR1.SetupAllProperties();
+            _mockRR2.SetupAllProperties();
+            _mockDb.Setup(add => add.Restrooms.Add(new Mock<Restroom>().Object));
+            _mockDb.Setup(save => save.SaveChanges());
+        }
+        [TestMethod()]
+        public void AddBathroomModelTest()
+        {
+            _mockDb.Object.Restrooms.Add(_mockRR2.Object);
+            _mockDb.Object.Restrooms.Add(_mockRR2.Object);
+            _mockDb.Object.SaveChanges();
+            Assert.IsNotNull(_mockDb);
+            Assert.IsNotNull(_mockRR1.Object.id);
+            Assert.IsNotNull(_mockDb.Object.Restrooms.Find(_mockRR1.Object.id));
+            Assert.IsNotNull(_mockDb.Object.Restrooms.Find(_mockRR2.Object.id));
+        }
+
+    */
